80de7832d67eba5d10613c10ca4580583a016dc9
日蚀 <jiaji.hjj@alipay.com>
2019-09-05 19:59:02 +0800

    update readme

--- src/main/java/subtitle/Main.java
+++ src/main/java/subtitle/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -14,7 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainWindow.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("MainWindow.fxml")));
         primaryStage.setTitle("Subtitle Bingo");
         primaryStage.setScene(new Scene(root, 300, 320));
         primaryStage.setMinWidth(800);
